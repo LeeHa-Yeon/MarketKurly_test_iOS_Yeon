80c9833dd0f9b3d45f5f8661fc71ec413d91cdb6
LeeHa-Yeon <gkdus1613@naver.com>
2022-01-19 10:00:51 +0900

    [FEAT] 전체등급보기, 다음달 예상등급 보기 구현

---
+++ MarketKurlyApp/MarketKurlyApp/Sources/Views/MyKurly/UserInfo/Level/NextLevelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextLevelViewController.swift
+//  MarketKurlyApp
+//
+//  Created by 이하연 on 2022/01/19.
+//
+
+import UIKit
+
+class NextLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
